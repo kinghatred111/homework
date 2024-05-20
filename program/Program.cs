c1044a49455b625889d30cd49af7af6bd428494b
JeCoder1112 <137323373+JeCoder1112@users.noreply.github.com>
2024-05-20 23:03:43 +0300

    Написание программы , решающую поставленную задачу.

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод исходного массива с клавиатуры
+        Console.WriteLine("Введите элементы исходного массива через запятую:");
+        string[] inputArray = Console.ReadLine().Split(',');
+
+        // Создание нового массива
+        string[] newArray = new string[inputArray.Length];
+        int newIndex = 0;
+
+        // Формирование нового массива
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                newArray[newIndex] = inputArray[i];
+                newIndex++;
+            }
+        }
+
+        // Вывод нового массива
+        Console.WriteLine("Новый массив:");
+        for (int i = 0; i < newIndex; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
